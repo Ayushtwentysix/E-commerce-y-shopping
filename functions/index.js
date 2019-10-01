c4f9aeb6f1462312b98b25fdcabda8bb9a5949c3
root <gupta.ayush1997@gmail.com>
2019-10-01 09:46:36 +0000

    Written dependencies in index.js file

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,18 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
+const express = require('express');
+const engines = require('consolidate');
+const session = require("express-session");
+ const FirebaseStore = require('connect-session-firebase')(session);
+const app = express();
+var firebase_web = require("firebase");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+var bodyParser =require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.engine('hbs', engines.handlebars);
+app.set('views','./views');
+app.set('view engine', 'hbs');
+app.enable('view cache');
+var handlebars = require('handlebars');
