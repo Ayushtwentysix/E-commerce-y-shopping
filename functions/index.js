2a5c12758b984e585183741226d6f6c22be47414
root <gupta.ayush1997@gmail.com>
2019-10-18 17:02:19 +0000

    add function cart

--- functions/index.js
+++ functions/index.js
@@ -31,6 +31,39 @@ appSeven.use(session({
     }));
 appSeven.use(cookieParser('mysupersecret'));
 
+function Cart(oldCart) {
+      oldCart = oldCart ? oldCart : {};
+    
+    this.items = oldCart.items || {};
+    this.totalQty = oldCart.totalQty || 0;
+    this.totalPrice = oldCart.totalPrice || 0;
+    
+      this.add = function(item, id) {
+        var storedItem = this.items[id];
+        if (!storedItem) {
+            storedItem = this.items[id] = {item: item, qty: 0, price: 0};
+        }
+        storedItem.qty++;
+        storedItem.price = storedItem.item.price;
+        this.totalQty++;
+        this.totalPrice += storedItem.item.price;
+    };
+    
+       this.removeItem = function(id) {
+        this.totalQty -= this.items[id].qty;
+        this.totalPrice -= this.items[id].price;
+        delete this.items[id];
+    };
+    
+        this.generateArray = function() {
+        var arr = [];
+        for (var id in this.items) {
+            arr.push(this.items[id]);
+        }
+        return arr;
+    };
+}
+
 app.get("/",(request, response) =>{    
 res.send("hello");     
 });
